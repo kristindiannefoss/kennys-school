92db789841c06d01abdce9bf2891f74d9ec73bdf
Kris Foss <kris.foss@gmail.com>
2016-04-28 14:19:52 -0600

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'teachers#index'
 
   resources :teachers, only: [:index, :show]
-  resources :students, only: [:index, :show]
+  resources :students, only: [:index]
 
   namespace "danger" do
     resources :teachers, only: [:index]
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,53 +10,61 @@
     last_name: "Lockman",
     age: 93,
     current_score: 9,
-    magical: false)
+    magical: false,
+    teacher_id: 1)
 
     Student.create(teacher_id: 2,
     first_name: "Rosa",
     last_name: "Streich",
     age: 48,
     current_score: 9,
-    magical: false )
+    magical: false ,
+    teacher_id: 2)
 
     Student.create(teacher_id: 2,
     first_name: "Elnora",
     last_name: "Anderson",
     age: 64,
     current_score: 8,
-    magical: true )
+    magical: true ,
+    teacher_id: 3)
 
     Student.create(teacher_id: 2,
     first_name: "Shayna",
     last_name: "Mann",
     age: 96,
     current_score: 7,
-    magical: false)
+    magical: false,
+    teacher_id: 4)
 
     Student.create(teacher_id: 3,
     first_name: "Kirk",
     last_name: "Parisian",
     age: 76,
     current_score: 5,
-    magical: false)
+    magical: false,
+    teacher_id: 1)
 
     Student.create(teacher_id: 3,
     first_name: "Marley",
     last_name: "Wiegand",
     age: 41,
     current_score: 2,
-    magical: true)
+    magical: true,
+    teacher_id: 2)
 
     Student.create(teacher_id: 3,
     first_name: "Jamaal",
     last_name: "Weissnat",
     age: 54,
     current_score: 3,
-    magical: true)
+    magical: true,
+    teacher_id: 5)
 
     Student.create(teacher_id: 1,
     first_name: "Aimee",
     last_name: "Moen",
     age: 51,
     current_score: 1,
-    magical: false )
+    magical: false ,
+    teacher_id: 5)
